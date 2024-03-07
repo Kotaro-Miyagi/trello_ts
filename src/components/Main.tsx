31074b32165614bfbd588463815e8e2f4fd2603d
Kotaro-Miyagi <miyagi.bad@icloud.com>
2024-03-08 04:49:15 +0900

    unable to resolve errors in Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
+import { DragDropContext, Droppable, Draggable, DropResult } from "react-beautiful-dnd";
+import dummyData from "../dummyData"
+
 
 const Main: React.FC = () => {
+    const [data, setData] = useState(dummyData)
+
+    const onDragEnd = (result: DropResult) => {
+
+    };
+
     return (
-        <div>Main</div>
+        <DragDropContext onDragEnd={onDragEnd}>
+            <div className={"trello"}>
+                {data.map((section) => (
+                    <Droppable key={section.id} droppableId={section.id}>
+                        {(provided) => <div className="trello-section" ref={provided.innerRef}>
+                            <div className="trello-section-title"></div>
+                        </div>}
+                    </Droppable>
+                ))}
+            </div>
+        </DragDropContext>
     );
 }
 
