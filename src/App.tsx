9366b8204eb4ffce1ca398dc58398263b54dc5a2
Kotaro-Miyagi <miyagi.bad@icloud.com>
2024-03-08 03:52:43 +0900

    edit css

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div style={{ padding: "50px"}}>
+      <h1 style={{ marginBottom: "20px"}}>Trello app</h1>
     </div>
   );
 }
