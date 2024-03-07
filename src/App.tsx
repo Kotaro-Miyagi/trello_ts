3dff08642a8da682535879fa906a01b769a1f687
Kotaro-Miyagi <miyagi.bad@icloud.com>
2024-03-08 04:26:37 +0900

    to display Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Main from './components/Main';
 
 function App() {
   return (
     <div style={{ padding: "50px"}}>
       <h1 style={{ marginBottom: "20px"}}>Trello app</h1>
+        <Main />
     </div>
   );
 }
